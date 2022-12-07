b3bea78336ecdcc2810f4a00bf77381b8eeb4b96
roopamrv <roopamverma9717@gmail.com>
2022-12-07 13:12:31 -0600

    Merged ServiceNow

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 import os
+import requests
+import json
 
 app = Flask(__name__)
 app.secret_key = 'your secret key'
@@ -17,6 +19,7 @@ app.config['MYSQL_USER'] = os.environ.get('CLOUD_SQL_USERNAME')
 app.config['MYSQL_PASSWORD'] = os.environ.get('CLOUD_SQL_PASSWORD')
 app.config['MYSQL_DB'] = os.environ.get('CLOUD_SQL_DATABASE_NAME')
 
+stop_words={'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'}
 
 mysql = MySQL(app)
 
@@ -39,7 +42,7 @@ def login():
             msg = 'Logged in successfully !'
             return redirect(url_for('posts', msg=msg))
         else:
-            msg = 'Incorrect username / password !'
+            msg = 'Incorrect username or password !'
             return render_template('login.html', msg=msg)
     return render_template('login.html', msg=msg)
 
@@ -89,6 +92,32 @@ def posts(opt):
         posts = conn.fetchall()
         for element in posts:
             data = []
+            sys_data = []
+            word_tokens = element['title'].split()
+            filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
+            filtered_sentence = word_tokens[0]
+            for w in word_tokens:
+                if w not in stop_words:
+                    filtered_sentence = filtered_sentence + "," + w
+                    filtered_sentence = filtered_sentence.replace(".", "")
+                    filtered_sentence = filtered_sentence.replace(",,", ",")
+            url = "https://us-central1-fair-gist-370123.cloudfunctions.net/get-sn-suggestions"
+            payload = json.dumps({
+                "keywords": filtered_sentence
+            })
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            response = requests.request("POST", url, headers=headers, data=payload)
+            res = response.json()
+            print(res)
+            if(len(res["result"])>0):
+                sys_reply=[]
+                sys_reply.append(res["result"][0]["content"])
+                sys_reply.append("System")
+                sys_reply.append(res["result"][0]["last_updated_on"])
+                sys_data.append(sys_reply)
+            element["sys_data"] = sys_data
             id_post = element['id']
             query_string = "SELECT a.postreply, a.post_id, a.timestamp, b.userid  FROM replies a INNER JOIN accounts b on b.id = a.account_id " \
                            "where post_id="+str(id_post)+" ORDER BY a.timestamp"
@@ -101,8 +130,8 @@ def posts(opt):
                 time = tup['timestamp']
                 reply.append(time)
                 data.append(reply)
-
             element["data"] = data
+        print(posts)
         return render_template('posts.html', posts=posts)
     else:
         msg = 'Error loading Discussion Board, please try again'
